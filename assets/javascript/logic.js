8cbe8984cb7e02af56f079bbcc291277d39ced18
Joshua Dulin <jzdulin@Joshuas-MBP-2.lan>
2018-03-12 20:00:37 -0400

    added firebase link

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyA4zp-3sHM0EPEIkCSYv6JOvKrDuY1fXE8",
+    authDomain: "trains-49e61.firebaseapp.com",
+    databaseURL: "https://trains-49e61.firebaseio.com",
+    projectId: "trains-49e61",
+    storageBucket: "trains-49e61.appspot.com",
+    messagingSenderId: "389899048009"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
