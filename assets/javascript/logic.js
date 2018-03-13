460a0b49bca2d40982222a8650091670b87445a6
Joshua Dulin <jzdulin@Joshuas-MBP-2.lan>
2018-03-12 20:25:04 -0400

    added javascript to take imput from html and put it in firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+//Firebase
 var config = {
     apiKey: "AIzaSyA4zp-3sHM0EPEIkCSYv6JOvKrDuY1fXE8",
     authDomain: "trains-49e61.firebaseapp.com",
@@ -9,3 +10,41 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  //Adding new trains
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainFirst = $("#first-train-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+    //var trainFirst = moment($("#first-train-input").val().trim(), "DD/MM/YY").format("X");
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      first: trainFirst,
+      frequency: trainFrequency
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.first);
+    console.log(newTrain.frequency);
+  
+    // Alert
+    alert("train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+  });
